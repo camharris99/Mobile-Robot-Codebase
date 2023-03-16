fa9259a1617460f9bd21486af176616746824049
pi <harricam@umich.edu>
2023-03-16 13:08:49 -0400

    adding everything so far

--- mbot/mbot_autonomy/src/mbot/drive_square.cpp
+++ mbot/mbot_autonomy/src/mbot/drive_square.cpp
@@ -1,6 +1,7 @@
 #include <common_utils/lcm_config.h>
 #include <mbot/mbot_channels.h>
-#include <lcmtypes/robot_path_t.hpp>
+#include <mbot_lcm_msgs/pose_xyt_t.hpp>
+#include <mbot_lcm_msgs/robot_path_t.hpp>
 #include <lcm/lcm-cpp.hpp>
 #include <iostream>
 #include <unistd.h>
@@ -17,12 +18,12 @@ int main(int argc, char** argv)
     
     std::cout << "Commanding robot to drive around 1m square " << numTimes << " times.\n";
     
-    robot_path_t path;
+    mbot_lcm_msgs::robot_path_t path;
     path.path.resize(numTimes * 4);
     
-    pose_xyt_t nextPose;
+    mbot_lcm_msgs::pose_xyt_t nextPose;
     
-    nextPose.x = 1.0f;
+    nextPose.x = 0.61f;
     nextPose.y = 0.0f;
     nextPose.theta = 0.0f;
     for(int n = 0; n < numTimes; ++n)
@@ -30,24 +31,24 @@ int main(int argc, char** argv)
         path.path[4*n] = nextPose;
     }
     
-    nextPose.x = 1.0f;
-    nextPose.y = 1.0f;
+    nextPose.x = 0.61f;
+    nextPose.y = -0.61f;
     nextPose.theta = 0.0f;
     for(int n = 0; n < numTimes; ++n)
     {
         path.path[4*n + 1] = nextPose;
     }
     
-    nextPose.x = 0.0f;
-    nextPose.y = 1.0f;
+    nextPose.x = 1.22f;
+    nextPose.y = -0.61f;
     nextPose.theta = 0.0f;
     for(int n = 0; n < numTimes; ++n)
     {
         path.path[4*n + 2] = nextPose;
     }
     
-    nextPose.x = 0.0f;
-    nextPose.y = 0.0f;
+    nextPose.x = 1.22f;
+    nextPose.y = 0.61f;
     nextPose.theta = 0.0f;
     for(int n = 0; n < numTimes; ++n)
     {
